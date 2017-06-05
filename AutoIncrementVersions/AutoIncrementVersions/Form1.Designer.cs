8e2eb510e9c97e5a4f52933316f35ffbf4c8c5ae
IgorvSoares <igorvsoarespt@gmail.com>
2017-06-05 15:25:45 +0100

    Add files via upload

---
+++ AutoIncrementVersions/AutoIncrementVersions/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AutoIncrementVersions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVersion = new System.Windows.Forms.Label();
+            this.lblLastBuilt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Version";
+            // 
+            // lblVersion
+            // 
+            this.lblVersion.AutoSize = true;
+            this.lblVersion.Location = new System.Drawing.Point(205, 142);
+            this.lblVersion.Name = "lblVersion";
+            this.lblVersion.Size = new System.Drawing.Size(35, 13);
+            this.lblVersion.TabIndex = 1;
+            this.lblVersion.Text = "label2";
+            // 
+            // lblLastBuilt
+            // 
+            this.lblLastBuilt.AutoSize = true;
+            this.lblLastBuilt.Location = new System.Drawing.Point(94, 220);
+            this.lblLastBuilt.Name = "lblLastBuilt";
+            this.lblLastBuilt.Size = new System.Drawing.Size(13, 13);
+            this.lblLastBuilt.TabIndex = 2;
+            this.lblLastBuilt.Text = "d";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 460);
+            this.Controls.Add(this.lblLastBuilt);
+            this.Controls.Add(this.lblVersion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVersion;
+        private System.Windows.Forms.Label lblLastBuilt;
+    }
+}
+
---
+++ AutoIncrementVersions/AutoIncrementVersions/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AutoIncrementVersions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            // Concise way to do this suggested by james_carter:
+            lblVersion.Text = Application.ProductVersion;
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+           
+
+        }
+    }
+}
